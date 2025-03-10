4f6412950e8bf441c721e2daf3f4a3d600546ba5
yaraca <a22310266@live.ceti.mx>
2025-03-10 00:05:26 -0600

    argumentos de palabras clave

---
+++ 0003_Argumentos_de_Palabras_Clave.py
@@ -0,0 +1,15 @@
+#print con múltiples argumentos
+#pone un espacio entre cada argumento
+#imprime los argumentos en una sola línea
+print("Las bestias", "raras", "se", "encontraban", "hambrientas")
+
+#argumento de palabra clave end=""
+print ("El heroé", "no tan valiente", "perdio.", end="") #continuación en la misma linea
+print(" Las bestias raras", "se", "lo", "comieron")
+print("Su nombre era", end="") #continuación en la misma linea
+print(" Chullito.")
+
+#argumento de palabra clave sep="-"
+print("Las", "bestias", "ganaron.", sep="-") #separado por un -
+print("El", "mal", "tal", "vez,", sep="*", end="")#continua en la misma linea separado por *
+print(" triunfó.")
