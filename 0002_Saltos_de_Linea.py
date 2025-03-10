6de7865b91474adbc6de22780fc9d0d745320fbc
yaraca <a22310266@live.ceti.mx>
2025-03-10 00:04:29 -0600

    dibujo de flechita

--- 0002_Saltos_de_Linea.py
+++ 0002_Saltos_de_Linea.py
@@ -4,7 +4,12 @@ print() #espacio vacío con print
 print("En un mundo muy extraño...")
 
 #salto de lína con caracter escape \n
-print("Donde se encontraban bestias raras...\n A las que todos temían...")
-print("\nY un día...")
+print("Donde se encontraban bestias raras...\n A las que todos temían...") #salto a linea siguiente
+print("\nY un día...")#salto de linea vacío
 print("\nUn héroe no tan valiente se enfrentó a ellas...")
 
+#dibujo de flecha con saltos de línea
+print("    *","\n","  * *" )
+print("  *   *", "\n", "*     *")
+print("***   ***", "\n", " *   *" )
+print("  *   *", "\n", " *****")
