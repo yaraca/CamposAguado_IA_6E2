5c1e94b1eaf66a59bf965115f6d1bc2829ac8c0c
yaraca <a22310266@live.ceti.mx>
2025-03-14 14:46:16 -0600

    Ejercicio final entradas y salidas simples

--- 0008_Entradas_Salidas_Simples.py
+++ 0008_Entradas_Salidas_Simples.py
@@ -1,5 +1,6 @@
+#Entradas y salidas simples
 #Función input
-#la función input() permite al usuario ingresar un valor
+# #la función input() permite al usuario ingresar un valor
 print ("Ingresa tu nombre:") 
 nombre = input() #asigna el nombre ingresado a la variable 
 print ("Hola,", nombre,"!") #imprime el nombre ingresado
@@ -21,3 +22,15 @@ print ()
 a = float(input("Ingresa el valor del primer cateto: ")) #permite ingresar un número
 b = float(input("Ingresa el valor del segundo cateto: ")) #permite ingresar un número
 print ("El valor de la hipotenusa es: ", (a**2+b**2)**0.5) #imprime el valor de la hipotenusa 
+print ()
+
+#Ejercicio de las entradas y salidas simples
+#Operaciones aritméticas básicas
+print ("Calculadora básica...")
+x = float(input("Ingresa el primer valor: ")) #ingresar el primer número
+y = float(input("Ingresa el segundo valor: ")) #Ingresar el segundo valor
+print ("La suma de los valores es: ", x + y) #Imprime la suma de los valores
+print ("La resta de los valores es: ", x - y) #Imprime la resta de los valores
+print ("La multiplicación de los valores es: ", x * y) #Imprime la multiplicación de los valores
+print ("La división de los valores es: ", x / y) #Imprime la división de los valores
+print ("Gracias por su preferencia...")
