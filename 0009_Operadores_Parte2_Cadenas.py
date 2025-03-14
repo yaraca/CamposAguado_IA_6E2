5a45c4bf90b3ae076be9d754b1e22a65cde1ea69
yaraca <a22310266@live.ceti.mx>
2025-03-14 15:24:56 -0600

    Ejercicio final de operadores

--- 0009_Operadores_Parte2_Cadenas.py
+++ 0009_Operadores_Parte2_Cadenas.py
@@ -12,3 +12,17 @@ print ("Dibujo de cuadradito")
 print ("+" + 10 * "-" + "+") #imprime el diseño del primer lado
 print (("|" + " " *10 + "|\n") * 6, end="") #imprime el diseño de los lados
 print ("+" + 10 * "-" + "+") #imprime el diseño del último lado
+print ()
+
+#Ejercicio Final Operadores y expresiones
+print ("Ejercicio final de operadores y expresiones")
+print ("¿A qué hora temina mi evento?...")
+hora = int(input("Ingresa la hora de inicio (horas): "))
+min = int(input("Ingresa los minutos de inicio (minutos):  "))
+duracion = int(input("Ingresa la duración del evento (minutos): "))
+min2 = min + duracion #suma los minutos de inicio más la duracion del evento
+hora2 = hora + min2 // 60 #suma la hora de inicio más los minutos totales
+min2 = min2 % 60 #calcula los minutos restantes
+hora2 = hora2 % 24 #calcula las horas restantes
+print ("El evento se termina a las... ", hora2, ":", min2, sep='')
+
