a331eef846d611f879a3d54c6bd05a8959f9d067
yaraca <a22310266@live.ceti.mx>
2025-03-14 10:47:52 -0600

    Variables

---
+++ 0006_Variables.py
@@ -0,0 +1,34 @@
+#Variables
+v = 5 #asigna el valor de 5 a la variable "v"
+dinero_cuenta = 1500
+nombre = "Yara"
+print (v) #imprimir valor de la variable a
+print (v, dinero_cuenta, nombre) #imprimir varias variables
+print (v + dinero_cuenta) #imprimir la suma de las variables
+print ("Dinero en las cuenta: " + str(v)) #convertir variable "a" a cadena para sumar
+print ()
+
+#asignación de variables 
+v2 = 15
+print (v2) #imprime el valor de b
+v2 = v2 + 60
+print (v2) #imprime el nuevo valor de b
+print ()
+
+#código para resolver el teorema de pitágoras
+a = 3
+b = 5
+c = (a**2 + b**2)**0.5 #realiza el calculo de la hipotenusa
+print ("c = " + str(c)) #muestra el resultado de la hipotenusa
+print ()
+
+#Ejercicio de variables (manzanas)
+juan = 3
+maria = 5
+adan = 6
+print ("Juan tiene: " + str(juan) + " manzanas") #imprime el número de manzanas de Juan
+print ("Maria tiene: " + str(maria) + " manzanas") #imprime el número de manzanas de Maria
+print ("Adán tiene: " + str(adan) + " manzanas") #imprime el número de manzanas de Adan
+print (juan, maria, adan) #imprime las variables 
+total_manzanas = juan + maria + adan #suma las variables 
+print ("Total de manzanas: " +str(total_manzanas)) # imprime el total de manzanas
