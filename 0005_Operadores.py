36177da3ceb8752b8e742b9a49ead3f05e661ec4
yaraca <a22310266@live.ceti.mx>
2025-03-14 10:15:37 -0600

    operadores

---
+++ 0005_Operadores.py
@@ -0,0 +1,15 @@
+#Operadores aritméticos
+print (5+10) #suma
+print (5-10) #resta
+print (5*10.) #multiplicación ent y float = float
+print (50/10) #división, siempre regresa un float
+print (5//10) #división entera, redondeo al entero más cercano 
+print (5%10) #módulo/residuo
+print (5**10) #potencia
+
+#Enlace de un operador 
+print (25%11%2) #modulo va de izq a der
+print (2**2**3) #exponencial va de der a izq
+
+#paréntesis
+print ((5 * ((25 % 11) + 15)/(4 * 6)) //2)
